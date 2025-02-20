035741d0d4672e91de2e008f81a8a2fd1c6b476f
Chetan Singh <heychetansingh@gmail.com>
2025-02-20 15:48:56 +0530

    Initial Commit

---
+++ Memetok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memetok
+//
+//  Created by Chetan Singh on 2025-02-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MemetokTests/MemetokTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemetokTests.swift
+//  MemetokTests
+//
+//  Created by Chetan Singh on 2025-02-20.
+//
+
+import Testing
+@testable import Memetok
+
+struct MemetokTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
